cec54e191ad30369921e0e0adf19ab731b010d1a
178inaba <178inaba@users.noreply.github.com>
2017-06-08 02:37:57 +0900

    Add GetPlayerStatus

--- nico.go
+++ nico.go
@@ -2,6 +2,7 @@ package nico
 
 import (
 	"context"
+	"encoding/xml"
 	"errors"
 	"net/http"
 	"net/url"
@@ -11,14 +12,16 @@ import (
 // Client is a API client for niconico.
 type Client struct {
 	http.Client
-	loginRawurl string
-	userSession string
+	loginRawurl    string
+	liveBaseRawurl string
+	userSession    string
 }
 
 // NewClient return new niconico client.
 func NewClient() *Client {
 	return &Client{
-		loginRawurl: "https://secure.nicovideo.jp/secure/login",
+		loginRawurl:    "https://secure.nicovideo.jp/secure/login",
+		liveBaseRawurl: "http://live.nicovideo.jp",
 	}
 }
 
@@ -53,4 +56,142 @@ func (c *Client) Login(ctx context.Context, mail, password string) error {
 	}
 
 	return errors.New("user session not found")
+}
+
+// PlayerStatus is niconico live player status.
+type PlayerStatus struct {
+	Status string `xml:"status,attr"`
+	Time   int64  `xml:"time,attr"`
+	Stream Stream `xml:"stream"`
+	User   struct {
+		UserID    string `xml:"user_id"`
+		Name      string `xml:"nickname"`
+		IsPremium int    `xml:"is_premium"`
+		RoomLabel string `xml:"room_label"`
+	} `xml:"user"`
+
+	Ms struct {
+		Addr   string `xml:"addr"`
+		Port   int    `xml:"port"`
+		Thread int64  `xml:"thread"`
+	} `xml:"ms"`
+}
+
+// Stream is niconico live player status in player status.
+type Stream struct {
+	ID                       string `xml:"id"`
+	Title                    string `xml:"title"`
+	Description              string `xml:"description"`
+	ProviderType             string `xml:"provider_type"`
+	DefaultCommunity         string `xml:"default_community"`
+	International            int64  `xml:"international"`
+	IsOwner                  int64  `xml:"is_owner"`
+	OwnerID                  int64  `xml:"owner_id"`
+	OwnerName                string `xml:"owner_name"`
+	IsReserved               int64  `xml:"is_reserved"`
+	IsNiconicoEnqueteEnabled int64  `xml:"is_niconico_enquete_enabled"`
+	WatchCount               int64  `xml:"watch_count"`
+	CommentCount             int64  `xml:"comment_count"`
+	BaseTime                 int64  `xml:"base_time"`
+	OpenTime                 int64  `xml:"open_time"`
+	StartTime                int64  `xml:"start_time"`
+	EndTime                  int64  `xml:"end_time"`
+	IsRerunStream            int64  `xml:"is_rerun_stream"`
+
+	// TODO
+	BourbonURL   interface{} `xml:"bourbon_url"`
+	FullVideo    interface{} `xml:"full_video"`
+	AfterVideo   interface{} `xml:"after_video"`
+	BeforeVideo  interface{} `xml:"before_video"`
+	KickoutVideo interface{} `xml:"kickout_video"`
+
+	TwitterTag       string `xml:"twitter_tag"`
+	DanjoCommentMode int64  `xml:"danjo_comment_mode"`
+	InfinityMode     int64  `xml:"infinity_mode"`
+	Archive          int64  `xml:"archive"`
+	Press            Press  `xml:"press"`
+
+	// TODO
+	PluginDelay interface{} `xml:"plugin_delay"`
+	PluginURL   interface{} `xml:"plugin_url"`
+	PluginURLs  interface{} `xml:"plugin_urls"`
+
+	AllowNetduetto               int64 `xml:"allow_netduetto"`
+	NgScoring                    int64 `xml:"ng_scoring"`
+	IsNonarchiveTimeshiftEnabled int64 `xml:"is_nonarchive_timeshift_enabled"`
+	IsTimeshiftReserved          int64 `xml:"is_timeshift_reserved"`
+	HeaderComment                int64 `xml:"header_comment"`
+	FooterComment                int64 `xml:"footer_comment"`
+	SplitBottom                  int64 `xml:"split_bottom"`
+	SplitTop                     int64 `xml:"split_top"`
+	BackgroundComment            int64 `xml:"background_comment"`
+
+	// TODO
+	FontScale interface{} `xml:"font_scale"`
+
+	CommentLock  int64        `xml:"comment_lock"`
+	Telop        Telop        `xml:"telop"`
+	ContentsList ContentsList `xml:"contents_list"`
+	PictureURL   string       `xml:"picture_url"`
+	ThumbURL     string       `xml:"thumb_url"`
+
+	// TODO
+	IsPriorityPrefecture interface{} `xml:"is_priority_prefecture"`
+}
+
+type Press struct {
+	DisplayLines int64 `xml:"display_lines"`
+	DisplayTime  int64 `xml:"display_time"`
+
+	// TODO
+	StyleConf interface{} `xml:"style_conf"`
+}
+
+type Telop struct {
+	Enable int64 `xml:"enable"`
+}
+
+type ContentsList struct {
+	// TODO slice?
+	Contents Contents `xml:"contents"`
+}
+
+type Contents struct {
+	ID           string `xml:"id,attr"`
+	DisableAudio int64  `xml:"disableAudio,attr"`
+	DisableVideo int64  `xml:"disableVideo,attr"`
+	StartTime    int64  `xml:"start_time,attr"`
+	Contents     string `xml:",chardata"`
+}
+
+func (c *Client) GetPlayerStatus(ctx context.Context, liveID string) (*PlayerStatus, error) {
+	u, err := url.Parse(c.liveBaseRawurl)
+	if err != nil {
+		return nil, err
+	}
+	u.Path = "api/getplayerstatus"
+
+	v := url.Values{}
+	v.Set("v", liveID)
+	u.RawQuery = v.Encode()
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	req.AddCookie(&http.Cookie{Name: "user_session", Value: c.userSession})
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	ps := PlayerStatus{}
+	if err := xml.NewDecoder(resp.Body).Decode(&ps); err != nil {
+		return nil, err
+	}
+
+	return &ps, nil
 }
