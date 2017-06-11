94e862a12bc66585c0b7123f19e3c2826349ab2d
178inaba <178inaba@users.noreply.github.com>
2017-06-11 19:37:30 +0900

    Add TestGetPlayerStatus

--- nico_test.go
+++ nico_test.go
@@ -2,6 +2,7 @@ package nico
 
 import (
 	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -22,5 +23,41 @@ func TestLogin(t *testing.T) {
 	}
 	if c.userSession != "foobarbaz" {
 		t.Fatalf("want %q but %q", "foobarbaz", c.userSession)
+	}
+}
+
+func TestGetPlayerStatus(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		us, err := r.Cookie("user_session")
+		if err != nil {
+			t.Fatalf("should not be fail: %v", err)
+		}
+		if us.Value != "user-session" {
+			t.Fatalf("want %q but %q", "user-session", us.Value)
+		}
+		v := r.URL.Query().Get("v")
+		if v != "lv123456789" {
+			t.Fatalf("want %q but %q", "lv123456789", v)
+		}
+		io.WriteString(w, `<?xml version="1.0" encoding="utf-8"?><getplayerstatus status="ok" time="1234567890"><stream><title>test-title</title></stream></getplayerstatus>`)
+	}))
+	defer ts.Close()
+
+	c := &Client{
+		liveBaseRawurl: ts.URL,
+		userSession:    "user-session",
+	}
+	ps, err := c.GetPlayerStatus(context.Background(), "lv123456789")
+	if err != nil {
+		t.Fatalf("should not be fail: %v", err)
+	}
+	if ps.Status != "ok" {
+		t.Fatalf("want %q but %q", "ok", ps.Status)
+	}
+	if ps.Time != 1234567890 {
+		t.Fatalf("want %d but %d", 1234567890, ps.Time)
+	}
+	if ps.Stream.Title != "test-title" {
+		t.Fatalf("want %q but %q", "test-title", ps.Stream.Title)
 	}
 }
